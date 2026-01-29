0aadfa2d8b872f02b6f4803ddef1f94374e2ec6d
David Ejindu <ejindudavid540@gmail.com>
2026-01-28 21:13:26 -0500

    Time: 23 ms (49.34%), Space: 19.4 MB (23.38%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -8,55 +8,45 @@ class Solution:
         if hashmap r - l + 1 == len(s1) increment l
 
         s2 = dcda
-             l
+              l
                 r   
 
 
         s1 = adc
 
         permutation_map = {a:1,d:1, c:1}
-        window = {d:2,c:1,a:1 }
-        have = 3
-        need = 3
+        window = {d:1,c:1, a:1 }
+        3
+        3
        
         """
 
-        s1_map = collections.defaultdict(int)
+        permutation_map = collections.defaultdict(int)
         window = collections.defaultdict(int)
         l = 0
-        
+        count = 0
 
 
         for char in s1:
-            s1_map[char] +=1
-
-        have,need = 0, len(s1_map)
+            permutation_map[char] +=1
 
         for r in range(len(s2)):
 
             window[s2[r]] +=1
 
-            if s2[r] in s1_map and window[s2[r]] == s1_map[s2[r]]:
-                have +=1
+            
+            
+           # print(window, permutation_map, len(s1), r - l + 1)
 
-            if r - l + 1 > len(s1):
-                left = s2[l]
-
-                if left in s1_map and window[left] == s1_map[left]:
-                    have -=1
-
-                window[left] -=1
-                if not window[left]:
-                    window.pop(left)
+            if r - l + 1 == len(s1) and not window == permutation_map:
+                window[s2[l]] -=1
+                if not window[s2[l]]:
+                    window.pop(s2[l])
                 l+=1
-            #print(have,need,s1_map,window,r - l + 1)
-            if have == need:
+                count -=1
+
+            if window == permutation_map:
                 return True
-
-
-            
-
-            
 
 
         return False
