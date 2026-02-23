ffd2227d1aeebc1a7ff061ace2fbe95568ac8027
David Ejindu <ejindudavid540@gmail.com>
2026-02-23 13:51:55 -0500

    Time: 27 ms (44.8%), Space: 25.9 MB (31.51%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,18 +1,54 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        result = [1] * len(nums)
-        prefix = 1
-        for i in range(len(nums)):
-            result[i] = prefix 
-            prefix = nums[i] * prefix
+        """
+        calculate the postfix
+        multiple that by the prefix to get the product of array except self
 
+        to get product of array except self
+        you must get product of values before it and after it
+        so prefix of [1,2,3,4] * the postfix of [1,2,3,4]
+
+                    [1,1,2,6]
+                    to get prefix start at index 1 since prefix of first element
+                    is 1 
+                    so would do for 1 in range(len(nums)):
+                    
+
+        [1,2,3,4]
+        
+        prefix
+        [1,1,2,6]
+
+        postfix
+        [24,12,4,1]
+
+        to get postfix we need to now do postfix = 1
+        loop backwards
+        postfix = 1
+        nums: [1,2,3,4]
+        prefix :[1,1,2,6]
+        for i in range(len(nums) -1, -1, -1)
+        prefix[i] *= postfix
+        postfix *= nums[i]
+
+
+        """
+    
+        prefix = [1] * len(nums)
+        prefix_val = 1
+        for i in range(1,len(nums)):
+            prefix_val *= nums[i-1]
+            prefix[i] *= prefix_val
+        print(prefix)
 
         postfix = 1
 
-        for i in range(len(nums) - 1, -1 , -1):
-            result[i] = result[i] * postfix
-            postfix = postfix * nums[i]
+        for i in range(len(nums) -1, -1, -1):
+            prefix[i] *= postfix
+            postfix *= nums[i]
 
-        return result 
+        return prefix
 
 
+
+      
