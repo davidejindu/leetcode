cfa1b0e1e9ecb0811b616a29800fc3259817061f
David Ejindu <ejindudavid540@gmail.com>
2026-02-23 09:51:40 -0500

    Time: 7 ms (53.15%), Space: 25.1 MB (5.83%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,21 +1,36 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = [[] for i in range(len(nums) + 1)]
+        """
 
-        countFrequency = {}
+        the most unique elements it can be is len of nums
+        since they can appear multiple times create a list where each 
+        element contains a list and append to that element
+        use a hashmap to keep count of how much that element appears
+        if it appears 5 times make it index 5 of the list 
+        then traverse through the list and keep append values until the result == k
+
+        """
+
+        frequency_list = [[] for i in range(len(nums) + 1)]
+
+
+        count_map = {}
 
         for num in nums:
-            countFrequency[num] = countFrequency.get(num, 0) + 1
+            count_map[num] = count_map.get(num,0) +1
 
-        for num, frequency in countFrequency.items():
-            freq[frequency].append(num)
 
-        
+        for num, count in count_map.items():
+            frequency_list[count].append(num)
+
+        print(frequency_list)
+
         result = []
-
-
-        for i in range(len(freq) - 1, -1, -1):
-            for num in freq[i]:
-                result.append(num)
-                if len(result) == k:
+        for list_index in range(len(frequency_list) - 1, -1, -1):
+            for val in frequency_list[list_index]:
+                result.append(val)
+                if k == len(result):
                     return result
+
+    
+        
