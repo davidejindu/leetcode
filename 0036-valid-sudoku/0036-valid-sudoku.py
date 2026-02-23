b05f514602d8f9ce8183a2d3d01a1b41dc03dda1
David Ejindu <ejindudavid540@gmail.com>
2026-02-23 15:01:08 -0500

    Time: 7 ms (31.63%), Space: 19.3 MB (62.01%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,18 +1,29 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rowSet = defaultdict(set)
-        colSet = defaultdict(set)
-        squareSet = defaultdict(set)
+        """
+        create a hashmap that has a hashset as value
+        for the hashmapa if the col or rows or squares in hashset return False
+        keep traversing
+        squares will be hashset key of r//3 c//3 
+        this is because if row is 2 2/3 == 0 so its in first row
+
+        """
+        row_map = collections.defaultdict(set)
+        col_map = collections.defaultdict(set)
+        square_map = collections.defaultdict(set)
 
         for r in range(9):
             for c in range(9):
                 if board[r][c] == ".":
                     continue
-                if (board[r][c] in rowSet[r]) or (board[r][c] in colSet[c]) or (board[r][c] in squareSet[(r//3,c//3)]):
-                    return False
 
-                rowSet[r].add(board[r][c])
-                colSet[c].add(board[r][c])
-                squareSet[(r//3,c//3)].add(board[r][c])
+                if (board[r][c] in row_map[r] or board[r][c] in col_map[c] 
+                    or board[r][c] in square_map[(r//3,c//3)]):
+
+                    return False
+                
+                row_map[r].add(board[r][c])
+                col_map[c].add(board[r][c])
+                square_map[(r//3,c//3)].add(board[r][c])
 
         return True
