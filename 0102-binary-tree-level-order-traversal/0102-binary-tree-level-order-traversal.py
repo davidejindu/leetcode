24e138b57afb66a5ac7089111ed3b1c6a56decec
David Ejindu <ejindudavid540@gmail.com>
2026-03-23 13:44:43 -0400

    Time: 0 ms (100%), Space: 20 MB (80.25%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -1,50 +1,38 @@
-from collections import deque
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         """
-        bfs does this exactly
-
-        make a queue
-        store the root as first value in queue
-
-        loop through len of queue 
-        pop first value in queue append it to list
-        then if first value has children add it to queue
-        after loop ends add list of appends to the result list
-        when queue is empty break out of main while loop and return result
-
+        make a queue FIFO
+        append root to it first
         """
 
 
-        if root is None:
-            return []
+        queue = collections.deque()
 
-
-        queue = deque([root])
-        result = []
-
-        while len(queue) > 0:
-
-            level_list = []
+        queue.append(root)
+        
+        level_order_list = []
+        while queue:
+            queue_list = []
             for _ in range(len(queue)):
-                node_in_queue = queue.popleft()
-                level_list.append(node_in_queue.val)
+                node = queue.popleft()
+                if node:
+                    queue_list.append(node.val)
+                    queue.append(node.left)
+                    queue.append(node.right)
 
-                if node_in_queue.left:
-                    queue.append(node_in_queue.left)
+            if queue_list:
+                level_order_list.append(queue_list)
 
-                if node_in_queue.right:
-                    queue.append(node_in_queue.right)
-
-            result.append(level_list)
-
-        return result
+        return level_order_list
 
 
+
+        
