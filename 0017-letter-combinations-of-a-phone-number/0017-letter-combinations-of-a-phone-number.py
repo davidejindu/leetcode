0554614a5dccdac436311fc31a80254decfaa3bd
David Ejindu <ejindudavid540@gmail.com>
2026-04-12 12:27:04 -0400

    Time: 0 ms (100%), Space: 19.3 MB (83.92%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,30 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        result = []
+
+        digitMap = {"2": "abc",
+                    "3": "def",
+                    "4": "ghi",
+                    "5": "jkl",
+                    "6": "mno",
+                    "7": "pqrs",
+                    "8": "tuv",
+                    "9": "wxyz"
+                    }
+
+        def backtrack(i, currString):
+            if len(currString) == len(digits):
+                result.append(currString)
+                return
+
+            for char in digitMap[digits[i]]:
+                backtrack(i + 1, currString + char)
+
+
+        if digits:
+            backtrack(0, "")
+
+        return result
+
+
+
