6fd41dd04043b095df7b3ea04cfc3707449ea84f
David Ejindu <ejindudavid540@gmail.com>
2026-05-05 20:49:20 -0400

    Time: 3 ms (52.24%), Space: 20.2 MB (67.02%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,25 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        """
-
-    2,7,11,15
-
-    so i want the indexes that add up to 9 
-    i can create a hashmap of value and index
-    if target - num in hashmap ik I have what i need
-    so i can return [hashmap[target-num], i]
-
-
-        """
-        target_map = {}
+        diff_map = {}
 
         for index, value in enumerate(nums):
-            num = target - value
+            diff = target - value
 
-            if num in target_map:
-                return [target_map[num], index]
+            if diff in diff_map:
+                return [diff_map[diff], index]
 
-            target_map[value] = index
+            diff_map[value] = index
 
-        print(target_map)
         return []
