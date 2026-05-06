277be4c83e8d700627a36e5a52039733691e7b63
David Ejindu <ejindudavid540@gmail.com>
2026-05-05 23:35:12 -0400

    Time: 11 ms (84.36%), Space: 23.9 MB (28.23%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,25 +1,24 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        """
+        loop through each character in word 
+        get the ascii and increment
+        make hashmap the ascii with numbers and the values the word
+
+
+
 
         """
-        loop through each word
-        loop through each character in word
-        make an array where its the length of the word 
-        then increment the value of the array
-        make a hashmap list where the key is a list and you append the word to the key
-        return a list of the values in hashmap
-        """
-
         anagram_map = defaultdict(list)
 
         for word in strs:
             count = [0] * 26
+
             for char in word:
-                count[ord(char) - ord('a')] +=1
+                count[ord(char) - ord("a")] +=1
 
             anagram_map[tuple(count)].append(word)
 
-
         return list(anagram_map.values())
-
+     
         
