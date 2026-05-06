8617426afbc8cffaf68256aba53d81e4c8cdfbbe
David Ejindu <ejindudavid540@gmail.com>
2026-05-06 14:59:12 -0400

    Time: 27 ms (44.32%), Space: 25.5 MB (54.75%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,54 +1,32 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        """
-        calculate the postfix
-        multiple that by the prefix to get the product of array except self
+        """"
+        must find the prefix and postfix of nums[i]
+        so create a prefix array itll be the same length but all 1
 
-        to get product of array except self
-        you must get product of values before it and after it
-        so prefix of [1,2,3,4] * the postfix of [1,2,3,4]
-
-                    [1,1,2,6]
-                    to get prefix start at index 1 since prefix of first element
-                    is 1 
-                    so would do for 1 in range(len(nums)):
-                    
+        [1,1,8,6]
+             i
 
         [1,2,3,4]
-        
-        prefix
-        [1,1,2,6]
+               i
+        start traversing at index 1 
+        nums[i] nums[i] * nums[i -1]
 
-        postfix
-        [24,12,4,1]
-
-        to get postfix we need to now do postfix = 1
-        loop backwards
-        postfix = 1
-        nums: [1,2,3,4]
-        prefix :[1,1,2,6]
-        for i in range(len(nums) -1, -1, -1)
-        prefix[i] *= postfix
-        postfix *= nums[i]
 
 
         """
-    
-        prefix = [1] * len(nums)
-        prefix_val = 1
-        for i in range(1,len(nums)):
-            prefix_val *= nums[i-1]
-            prefix[i] *= prefix_val
-        print(prefix)
 
-        postfix = 1
-
-        for i in range(len(nums) -1, -1, -1):
-            prefix[i] *= postfix
-            postfix *= nums[i]
-
-        return prefix
+        prefix_list = [1] * len(nums)
+        prefix = 1
+        for i in range(1, len(nums)):
+            prefix *= nums[i - 1] 
+            prefix_list[i] *= prefix
 
 
+        postfix = 1 
 
-      
+        for i in range(len(nums) -2, -1, -1):
+            postfix *= nums[i + 1]
+            prefix_list[i] *= postfix
+
+        return prefix_list
