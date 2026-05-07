dcc96384e5a2d93dd399e38abf4fd8c337880f11
David Ejindu <ejindudavid540@gmail.com>
2026-05-07 14:32:28 -0400

    Time: 0 ms (100%), Space: 19.3 MB (33.28%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,28 +5,38 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        create a dummy node to keep track of head of the new list
+        then with the curr that will be used to create a new list
         
+        loop while you have both nodes 
+        compare the two nodes if node1 is smaller than node 2
+        add curr.next to node1 and increment node1
+        else add node2 to curr.next increment node2
 
+        now if not node1 then set the whole rest of curr.next to that node
+
+
+        """
         dummy = ListNode()
-        merged_curr = dummy
+        curr = dummy
+        node1, node2 = list1, list2
 
-        curr1 = list1
-        curr2 = list2
-
-        while curr1 and curr2:
-            if curr1.val <= curr2.val:
-                merged_curr.next = curr1
-                curr1 = curr1.next
-
+        
+        while node1 and node2:
+            if node1.val > node2.val:
+                curr.next = node2
+                node2 = node2.next
             else:
-                merged_curr.next = curr2
-                curr2 = curr2.next
+                curr.next = node1
+                node1 = node1.next
 
-            merged_curr = merged_curr.next
+            curr = curr.next
 
-        if curr1 or curr2:
-            merged_curr.next = curr1 or curr2
+        if not node1:
+            curr.next = node2
+        else:
+            curr.next = node1
 
         return dummy.next
-                
 
