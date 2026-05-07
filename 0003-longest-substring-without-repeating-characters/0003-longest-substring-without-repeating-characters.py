0d373b61a69557fa809d8bd1af2a4c35c8cf048f
David Ejindu <ejindudavid540@gmail.com>
2026-05-06 20:44:39 -0400

    Time: 11 ms (77.11%), Space: 19.3 MB (31.09%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,33 +1,19 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        """
-        use a set to check if its in the window
-        l,r = 0, 0
-        if s[r] not in set append and increase the max
-        if its inset keep popping left until its not
-        
-        longest_substring = 2
-        in_substring = {d, v}
-        dvdf
-          r
-         l
-        """
-
+        max_length = l = 0
         substring_set = set()
-        longest_substring = 0
-        l = 0
 
         for r in range(len(s)):
-            if s[r] not in substring_set:
-                longest_substring = max(longest_substring, r - l + 1)
-                substring_set.add(s[r])
-            else:
-                while s[r] in substring_set:
-                    substring_set.remove(s[l])
-                    l +=1
-                substring_set.add(s[r])
-            print(substring_set)
+            while s[r] in substring_set:
+                substring_set.remove(s[l])
+                l +=1
 
-        return longest_substring
+            substring_set.add(s[r])
+            max_length = max(max_length, r - l + 1)
+
+        return max_length
+
+                
+       
 
             
