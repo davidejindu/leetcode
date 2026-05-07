3634f9e0d5ca70f52e1e20c2cc592e850b2860cf
David Ejindu <ejindudavid540@gmail.com>
2026-05-07 14:17:03 -0400

    Time: 0 ms (100%), Space: 19.6 MB (10.29%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -2,19 +2,30 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         """
 
+        4,5,6,7,0,1,2   target = 0
+                l
+                  m
+                r
+
+        so if mid is greater than r we know that the left right portion is sorted
+        increasing order 
+        so if target is less than r  its in that range
+        thus l = mid + 1
+        now if target is greater than r then set r = m because its in that range
+        
         """
 
 
         l, r = 0, len(nums) - 1
 
-        while l <=r:
-            mid = (l+r) // 2
+        while l <= r:
+            mid = (l + r) // 2
 
             if nums[mid] == target:
                 return mid
 
             if nums[mid] > nums[r]:
-                if target >= nums[l] and target < nums[mid]:
+                if nums[l] <= target and target < nums[mid] :
                     r = mid - 1
                 else:
                     l = mid + 1
@@ -23,15 +34,8 @@ class Solution:
                 if target > nums[mid] and target <= nums[r]:
                     l = mid + 1
                 else:
-                    r = mid -1
-
-        return -1 
+                    r = mid - 1
 
 
 
-
-
-       
-
-
-
+        return -1
