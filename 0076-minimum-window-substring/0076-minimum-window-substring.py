de9bf1756c21541d7985f6ab127da26cb474395a
David Ejindu <ejindudavid540@gmail.com>
2026-05-07 10:46:32 -0400

    Time: 136 ms (24.37%), Space: 19.8 MB (37.35%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,54 +1,49 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         """
-        s = "ADOBECODEBANC", t = "ABC"
-             l
-             r
 
-        countT = {A:1, B:1, C:1}
-        countS = {A: 1, B:2, C:1}
-        have = 3
-        need = 2
-        shouldn't have be sum not len?
-        if its len then it wont account for multiple key values
-
-
+        s = "ADOBECODEBANC", t = "ABC"  output = "BANC"
+                       l
+                         r
+             A:1, B:1, C:1
+             have = 3
+             need = 3
+             min_substring = "BANC" 4  
 
         """
+        if s == t:
+            return s
 
-        l = 0
-        result = [-1,-1]
-        minimum_substring = float('inf')
-        countT = defaultdict(int)
-        countS = defaultdict(int)
-
+        map_t = {}
+        
         for char in t:
-            countT[char] +=1
+            map_t[char] = 1 +map_t.get(char,0)
 
-        need, have = len(countT), 0
-
-
-
+        have,need = 0, len(map_t)
+        map_s = {}
+        min_substring = ""
+        l = 0
 
         for r in range(len(s)):
-            countS[s[r]] +=1
+            if s[r] in map_t:
+                map_s[s[r]] = 1 + map_s.get(s[r],0)
             
-            if s[r] in countT and countS[s[r]] == countT[s[r]]:
+            if s[r] in map_t and map_s[s[r]] == map_t[s[r]]:
                 have +=1
-
+    
             while have == need:
-                if r - l + 1 < minimum_substring:
-                    minimum_substring = r - l + 1
-                    result = [l,r]
+                if not len(min_substring):
+                    min_substring = s[l:r+1]
+                elif min_substring and r - l + 1 < len(min_substring):
+                    min_substring = s[l:r + 1]
 
-                countS[s[l]] -=1
+                if s[l] in map_s:
+                    map_s[s[l]] -=1
+                    if map_s[s[l]] < map_t[s[l]]:
+                        have -=1
+                l +=1 
 
-                if s[l] in countT and countS[s[l]] < countT[s[l]]:
-                    have -=1
-                l +=1
+        return min_substring
 
-        if minimum_substring == float('inf'):
-            return ""
-        else:
-            l, r = result
-            return s[l:r +1]
+
+
