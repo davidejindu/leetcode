310786bb7dd12b1d7dee3f05d069ba8465ffdd87
David Ejindu <ejindudavid540@gmail.com>
2026-05-08 09:26:53 -0400

    Time: 0 ms (100%), Space: 19.3 MB (93.01%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,30 +1,33 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         """
-    we know if m is greater than r then the minimum has to be in the left portion
 
-    3,4,5,1,2
-          l 
-        m
-          r
+    4,5,6,7,0,1,2
+            l       
+              m
+                r
 
-    if its not greater than it then m might be the minimum so set r to m to keep it in range
+    if mid > r that means the left portion contains higher values and pivot is to the right 
 
-
+    else 
+    that means the minimum can be mid or anything to left of mid so set r = mid
 
 
 
 
         """
 
+
         l, r = 0, len(nums) - 1
+
 
         while l < r:
             mid = (l + r) // 2
-
             if nums[mid] > nums[r]:
                 l = mid + 1
             else:
                 r = mid
 
-        return nums[r]
+        return nums[l]
+
+            
