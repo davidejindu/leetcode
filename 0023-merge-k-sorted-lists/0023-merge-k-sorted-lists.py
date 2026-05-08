0812388009370ca7099b0a6b71fbfde25c30153b
David Ejindu <ejindudavid540@gmail.com>
2026-05-08 14:48:50 -0400

    Time: 11 ms (55.51%), Space: 22.6 MB (93.34%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -6,63 +6,62 @@
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         """
-        lists = [1,4,5],[1,3,4],[2,6]
-                  i      i + 1
-
-        merged_list = [1,1,3,4,4,5]
-
-        lists = [1,4,5], [1,3,4], [2,6]
-                                    i     i + 1
-        merged_list = [1,1,3,4,4,5], [2,6]
+        basically merge sort
+        keep sorting two of the lists
+        then change lists = to the merged lists keep looping until 1 list
+        meaning you have merged all of it then return
 
 
-        lists = [1,1,3,4,4,5], [2,6]
+        [1,4,5],[1,3,4],[2,6]
+                          i
+
+        temp_ lists = [[1,1,3,4,4,5], [2,6]]
+        
+        lists = [[1,1,3,4,4,5], [2,6]]
                     i           i + 1
+        temp_lists = [1,1,2,3,4,4,5,6]
 
-        merged_list = [1,1,2,3,4,4,5,6]
+        lists = temp_lists
+
+      
+
 
 
         """
-
-        if not lists or len(lists) == 0:
+        if not lists:
             return None
 
-
         while len(lists) > 1:
-            merged_list = []
+            temp_lists = []
 
             for i in range(0, len(lists), 2):
                 l1 = lists[i]
-                l2 = lists[i + 1] if i+1 < len(lists) else None
-                merged_list.append(self.mergeTheList(l1,l2))
+                l2 = lists[i + 1] if (i + 1) < len(lists) else None
+                temp_lists.append(self.mergedList(l1,l2))
 
-            lists = merged_list
+            lists = temp_lists
 
         return lists[0]
 
 
 
-    def mergeTheList(self,l1,l2):
-        dummy = ListNode()
-        tail = dummy
+    def mergedList(self,l1,l2):
+        head = ListNode()
+        dummy = head
 
         while l1 and l2:
             if l1.val < l2.val:
-                tail.next = l1
+                dummy.next = l1
                 l1 = l1.next
-
             else:
-                tail.next = l2
+                dummy.next = l2
                 l2 = l2.next
 
-            tail = tail.next
+            dummy = dummy.next
 
-        if l1:
-            tail.next = l1
+        if not l1:
+            dummy.next = l2
         else:
-            tail.next = l2
+            dummy.next = l1
 
-        tail = tail.next
-
-        return dummy.next
-
+        return head.next
