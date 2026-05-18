8f1686b8a45549d9655a2782cd04dd65717e0699
David Ejindu <ejindudavid540@gmail.com>
2026-05-18 13:06:10 -0400

    Time: 86 ms (30.42%), Space: 90.8 MB (22.12%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,17 +6,29 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        """
+        preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+
+        so the root is always first index of preorder
+
+        so create tree where it starts root with preorder[0]
+
+        then we know everything to left of 3 in inorder is left subtree
+        everything to right of 3 in inorder is right
+
+        thus we should get index of preorder[0]
+
+        then recursve the root.left to make sure that it only captures left of it
+        then recurve the root.right to make sure that it only caputreus left of it
+        
+        
+        """
+
         if not preorder or not inorder:
             return None
 
-
         root = TreeNode(preorder[0])
         mid = inorder.index(preorder[0])
-
-
-        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid+1])
-        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
-
+        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:],inorder[mid+1:])
         return root
-
-        
