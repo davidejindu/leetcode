f92f8f135d323926079de927ec6f49485c2005f1
David Ejindu <ejindudavid540@gmail.com>
2026-05-24 17:37:38 -0400

    Time: 198 ms (42.66%), Space: 42.3 MB (30.82%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,57 +1,41 @@
 class MedianFinder:
 
-    """
-    medianFinder.addNum(1);    
-    medianFinder.addNum(2);    
-    medianFinder.findMedian();
-    medianFinder.addNum(3);  
-    medianFinder.addNum(0);  
-    medianFinder.addNum(7);  
-    medianFinder.findMedian(); 
-
-
-    small_heap = [-1,0]
-    large_heap = [2, , 7]
-
-
-    create a small_heap and a large_heap
-    they can't have more than 1 size length difference
-    if they do then pop whichever one is greater by 2 value and add it to other heap
-    small_heap is max heap and large_heap is min heap
-    also all of the values in large heap has to be greater than all of values in small heap
-
-
-    """
     def __init__(self):
-        self.small_heap = []
-        self.large_heap = []
-        heapq.heapify(self.small_heap)
-        heapq.heapify(self.large_heap)
+        self.largeHeap = []
+        self.smallHeap = []
         
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.small_heap, -num)
 
-        if self.small_heap and self.large_heap:
-            if -self.small_heap[0] > self.large_heap[0]:
-                heapq.heappush(self.large_heap, -heapq.heappop(self.small_heap))
+        heapq.heappush(self.smallHeap,-num)
 
-        if len(self.large_heap) - len(self.small_heap) > 1:
-            heapq.heappush(self.small_heap, -heapq.heappop(self.large_heap))
+        # check wrong values in heaps
+        if self.largeHeap and -self.smallHeap[0] > self.largeHeap[0]:
+            large_num = -heapq.heappop(self.smallHeap)
+            heapq.heappush(self.largeHeap,large_num)
 
-        if len(self.small_heap) - len(self.large_heap) > 1:
-            heapq.heappush(self.large_heap, -heapq.heappop(self.small_heap))
+        # check too large heaps
+        if len(self.smallHeap) - len(self.largeHeap) > 1:
+            val = -heapq.heappop(self.smallHeap)
+            heapq.heappush(self.largeHeap, val)
 
 
+        # chec if large heap is greater than small heap
+        if len(self.largeHeap) > len(self.smallHeap):
+            small_num = heapq.heappop(self.largeHeap)
+            heapq.heappush(self.smallHeap, -small_num)
 
+
+    
 
     def findMedian(self) -> float:
-        if len(self.small_heap) == len(self.large_heap):
-            return (-self.small_heap[0] + self.large_heap[0]) / 2
-        elif len(self.small_heap) > len(self.large_heap):
-            return -self.small_heap[0]
+        length = len(self.smallHeap) + len(self.largeHeap)
+
+        if length % 2 != 0:
+            return -self.smallHeap[0]
         else:
-            return self.large_heap[0]
+            return (-self.smallHeap[0] + self.largeHeap[0]) / 2
+
         
 
 
