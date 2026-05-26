8d0db92a8939b3f6f94dd4b654c38e9123572184
David Ejindu <ejindudavid540@gmail.com>
2026-05-26 14:08:01 -0400

    Time: 4530 ms (26%), Space: 19.5 MB (57.48%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,7 +1,23 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        """
+        go through each row,col and dfs check each direction
+        backtrack
+
+        store the row col in a set to make sure we dont backtrack into same
+        row col on same path
+
+        if i == len word that means we reached the word so return true
+
+        if 0 <= r or r > row same for col
+        or same r c in path or word[i] != to the board number then 
+        return false
+
         
 
+
+
+        """
         ROW, COL = len(board), len(board[0])
         path = set()
 
@@ -10,18 +26,18 @@ class Solution:
             if i == len(word):
                 return True
 
-            if(0 > r or r >= ROW or
-               0 > c or c >= COL or
-               (r,c) in path or
-               board[r][c] != word[i]):
-               return False
+            if (0 > r or r >= ROW or
+                0 > c or c >= COL or
+                (r,c) in path or
+                word[i] != board[r][c]):
+                return False
 
             path.add((r,c))
 
-            result = (dfs(i+1,r +1,c) or
-                      dfs(i+1,r -1,c) or
-                      dfs(i+1,r,c +1) or
-                      dfs(i+1,r,c-1))
+            result = (dfs(i+1,r+1,c) or
+                      dfs(i+1,r-1,c) or
+                      dfs(i+1,r,c+1) or
+                      dfs(i +1,r,c-1))
 
             path.remove((r,c))
             return result
