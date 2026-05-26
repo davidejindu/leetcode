df89f5b595f70652b0ceb9c62cf9cdf3bd1b15d4
David Ejindu <ejindudavid540@gmail.com>
2026-05-25 20:55:21 -0400

    Time: 181 ms (57.63%), Space: 41.6 MB (87.73%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -6,36 +6,31 @@ class MedianFinder:
         
 
     def addNum(self, num: int) -> None:
-
         heapq.heappush(self.smallHeap,-num)
 
-        # check wrong values in heaps
+        # check if small heap len is greater 
+        if len(self.smallHeap) - len(self.largeHeap) > 1:
+            large_num = -heapq.heappop(self.smallHeap)
+            heapq.heappush(self.largeHeap,large_num)
+ 
+        #swap small heap great value with large heap great value
         if self.largeHeap and -self.smallHeap[0] > self.largeHeap[0]:
             large_num = -heapq.heappop(self.smallHeap)
             heapq.heappush(self.largeHeap,large_num)
 
-        # check too large heaps
-        if len(self.smallHeap) - len(self.largeHeap) > 1:
-            val = -heapq.heappop(self.smallHeap)
-            heapq.heappush(self.largeHeap, val)
 
-
-        # chec if large heap is greater than small heap
+        #check if large heap len is greater
         if len(self.largeHeap) > len(self.smallHeap):
-            small_num = heapq.heappop(self.largeHeap)
-            heapq.heappush(self.smallHeap, -small_num)
-
-
-    
+            small_num = -heapq.heappop(self.largeHeap)
+            heapq.heappush(self.smallHeap,small_num)
+        
 
     def findMedian(self) -> float:
-        length = len(self.smallHeap) + len(self.largeHeap)
 
-        if length % 2 != 0:
-            return -self.smallHeap[0]
+        if (len(self.largeHeap) + len(self.smallHeap)) % 2 == 0:
+            return (self.largeHeap[0] + -self.smallHeap[0]) / 2
         else:
-            return (-self.smallHeap[0] + self.largeHeap[0]) / 2
-
+            return -self.smallHeap[0]
         
 
 
