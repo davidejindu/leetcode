9ebad04f22fe6fc0e1079ba74be860b3a6573fc3
David Ejindu <ejindudavid540@gmail.com>
2026-06-09 19:17:06 -0400

    Time: 251 ms (66.73%), Space: 19.5 MB (20.48%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,59 +1,28 @@
-"""
-
-input is a string need to return longest palindrome
-all of charcters are digiest and english letters
-
-input is a string
-output is string of longest palindrome
-
-input = babad
-        brute force O(n^2) is a double for loop
-
-        at each character in input check if its a palindrome
-
-
-result = bab
-
-        babad
-          i
-           j
-
-        bab
-
-
-
-"""
-
-
-
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         longest_palindrome = ""
-        max_length = -1
+        max_length = 0
 
-    
-        def isPalindrome(l, r):
-            while l < r:
-                if s[l] != s[r]:
-                    return False
+        def expand(l,r):
+            while 0 <= l and r < len(s) and s[l] == s[r]:
+                l -=1
+                r +=1
 
-                l +=1
-                r -=1
-
-            return True
+            return s[l+1:r]
 
         for i in range(len(s)):
 
-            for j in range(i,len(s)):
-                if isPalindrome(i,j) and j - i + 1 > max_length:
-                    longest_palindrome = s[i:j + 1]
-                    max_length = j - i + 1
+            odd = expand(i,i)
+            even = expand(i, i + 1)
 
-            if len(s) - i <= max_length:
-                return longest_palindrome
-
+            if len(odd) > len(longest_palindrome):
+                longest_palindrome = odd
             
+            if len(even) > len(longest_palindrome):
+                longest_palindrome = even
 
+        return longest_palindrome
+            
 
 
         
