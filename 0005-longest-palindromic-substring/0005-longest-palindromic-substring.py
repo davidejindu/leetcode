8f346e590e92efa2d74e400ed5d063a0d5b50822
David Ejindu <ejindudavid540@gmail.com>
2026-06-09 18:46:11 -0400

    Time: 3569 ms (15.8%), Space: 19.3 MB (37.68%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,39 +1,59 @@
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        res = " "
-        resLen = 0
+"""
+
+input is a string need to return longest palindrome
+all of charcters are digiest and english letters
+
+input is a string
+output is string of longest palindrome
+
+input = babad
+        brute force O(n^2) is a double for loop
+
+        at each character in input check if its a palindrome
 
 
-        """
+result = bab
+
         babad
           i
-          i
+           j
 
-        cbbd
+        bab
 
-        """
 
+
+"""
+
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        longest_palindrome = ""
+        max_length = -1
+
+    
+        def isPalindrome(l, r):
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+
+                l +=1
+                r -=1
+
+            return True
 
         for i in range(len(s)):
-            #odd length
-            l, r = i, i
 
-            while 0 <= l and r < len(s) and s[l] == s[r]:
-                if (r - l +1) > resLen:
-                    res = s[l:r+1]
-                    resLen = r - l + 1
+            for j in range(i,len(s)):
+                if isPalindrome(i,j) and j - i + 1 > max_length:
+                    longest_palindrome = s[i:j + 1]
+                    max_length = j - i + 1
 
-                l -=1
-                r +=1
+            if len(s) - i <= max_length:
+                return longest_palindrome
 
-            l, r = i, i +1 
+            
 
-            while 0 <= l and r < len(s) and s[l] == s[r]:
-                if (r - l +1) > resLen:
-                    res = s[l:r+1]
-                    resLen = r - l + 1
 
-                l -=1
-                r +=1
 
-        return res
+        
