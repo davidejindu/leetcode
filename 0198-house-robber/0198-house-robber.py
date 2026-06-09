cef0259bfbe990281d4ef76b598ec83160b312b4
David Ejindu <ejindudavid540@gmail.com>
2026-06-08 22:00:17 -0400

    Time: 0 ms (100%), Space: 19.3 MB (61.57%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,18 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        """
+        if len(nums) == 1:
+            return nums[0]
 
-        1,3,3,1,0,0
-        1
-        3
-        4
+        adj1 = nums[0]
+        adj2 = max(nums[0], nums[1])
 
+        for i in range(2, len(nums)):
+            current = max(nums[i] + adj1, adj2)
 
-        """
-        nums.append(0)
-        nums.append(0)
+            adj1 = adj2
+            adj2 = current 
 
-        for i in range(len(nums) -3, -1, -1):
-            nums[i] = max(nums[i] + nums[i + 2], nums[i + 1])
-
-        return nums[0]
+        return adj2
