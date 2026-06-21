a666750cde8a5ed6922203daf4e18b571bdca5ea
David Ejindu <ejindudavid540@gmail.com>
2026-06-21 13:54:41 -0400

    Time: 12 ms (14.14%), Space: 24.8 MB (12.4%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,36 +1,39 @@
+"""
+return the the most k freq elements 
+make an array that hold where the max frequency possible
+
+inside of each element append to the index if it matches the freq the  element appears
+
+get that by creating a hashmap of the number with the value being its freq
+
+
+after you store each number in the correct freq loop backwards until you have 
+appended k elements to the result
+
+
+
+"""
+
+
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        """
+        freq = [[] for i in range(len(nums) + 1)]
 
-        the most unique elements it can be is len of nums
-        since they can appear multiple times create a list where each 
-        element contains a list and append to that element
-        use a hashmap to keep count of how much that element appears
-        if it appears 5 times make it index 5 of the list 
-        then traverse through the list and keep append values until the result == k
-
-        """
-
-        frequency_list = [[] for i in range(len(nums) + 1)]
-
-
-        count_map = {}
+        freq_map = {}
 
         for num in nums:
-            count_map[num] = count_map.get(num,0) +1
+            freq_map[num] = 1 + freq_map.get(num, 0)
 
-
-        for num, count in count_map.items():
-            frequency_list[count].append(num)
-
-        print(frequency_list)
+        for num, count in freq_map.items():
+            freq[count].append(num)
 
         result = []
-        for list_index in range(len(frequency_list) - 1, -1, -1):
-            for val in frequency_list[list_index]:
-                result.append(val)
-                if k == len(result):
+
+        for i in range(len(nums), -1, -1):
+            for nums in freq[i]:
+                result.append(nums)
+                if len(result) == k:
                     return result
 
-    
         
