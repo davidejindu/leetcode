1370e147eeec0bddf815137443407fc719d5e88f
David Ejindu <ejindudavid540@gmail.com>
2026-06-22 12:48:55 -0400

    Time: 59 ms (52.17%), Space: 29.6 MB (67.86%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,25 +1,19 @@
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        """
-        two pointers 
-        get the min height between two points then multiple the difference
-        store that in max variable and proceed to go to whatever one is smaller
-
-
-        """
-
-        max_area = 0
-        l, r = 0, len(height) - 1
+        maxHeight = 0
+        l,r = 0, len(height) - 1
 
         while l < r:
-            min_height = min(height[l], height[r])
-            curr_height = min_height * (r - l)
+            width = r - l 
+            minHeight = min(height[l], height[r]) 
 
-            max_area = max(max_area, curr_height)
+            maxHeight = max(maxHeight, minHeight * width)
 
             if height[l] < height[r]:
                 l +=1
             else:
                 r -=1
 
-        return max_area
+        return maxHeight
+        
