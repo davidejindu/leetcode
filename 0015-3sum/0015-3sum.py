8dd5b3b228d6045a4047b51df9fa6fad20182f7e
David Ejindu <ejindudavid540@gmail.com>
2026-06-22 12:42:13 -0400

    Time: 638 ms (56.99%), Space: 22.2 MB (54.14%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,45 +1,57 @@
+"""
+-1,0,1,2,-1,-4
+
+-4,-1,-1,0,1,2
+    i
+         l
+           r
+0
+[-1,-1,2]
+
+two pointers but set one pointer at the beginning
+
+make sure the first pointer is never the same as the previous that might cause duplicates
+
+when doing two pointers after getting a val that is 0 append it but then you 
+after getting target check if the l -1 == l if so you have to move l pointer again or duplicate
+everytime you get the target increment
+
+also if l is < 0 increment if it is greater decrement the r
+"""
+
+
+
+
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
-        """
-        -1,0,1,2,-1,-4
-        doing two pointers and since we dont want duplicates sort so we can skip over same values
-
-        0,0,0
-         i
-                l
-                    r
-
-        value = -1 
-        first-value = [-1,-1,2], 
-
-
-
-        """
-
-        result = []
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
+        result = []
 
         for i in range(len(nums)):
-            l, r  = i + 1, len(nums) - 1
-
-            if i > 0 and nums[i] == nums[i -1]:
+            #never want to start off with same first value to prevent duplicate
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
 
+            l = i + 1
+            r = len(nums) - 1
+
             while l < r:
-                if nums[i] + nums[l] + nums[r] == 0:
+                val = nums[i] + nums[l] + nums[r]
+
+                if val == 0:
                     result.append([nums[i],nums[l],nums[r]])
                     l +=1
-
                     while l < r and nums[l] == nums[l - 1]:
                         l +=1
-                    
 
-                if nums[i] + nums[l] + nums[r] < 0 :
+                elif val < 0:
                     l +=1
-                else: r -=1
+                else:
+                    r -=1
 
-              
             
 
         return result
-            
+
+
+        
