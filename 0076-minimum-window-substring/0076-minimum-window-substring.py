7da39bd49ffdc3258406185828c30d66abb139d1
David Ejindu <ejindudavid540@gmail.com>
2026-06-23 10:56:52 -0400

    Time: 122 ms (34.21%), Space: 19.6 MB (73.25%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,49 +1,51 @@
+"""
+aaaaaaaaaaaabbbbbcdd   abcdd
+l  
+                   r
+
+t = {A:1, B:1, C:1, D:2}
+s = {A:12, B:5, C:1, D:2}
+min_substring = "" 
+substring_len = inf
+have = 4
+need = 4
+
+"""
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        """
-
-        s = "ADOBECODEBANC", t = "ABC"  output = "BANC"
-                       l
-                         r
-             A:1, B:1, C:1
-             have = 3
-             need = 3
-             min_substring = "BANC" 4  
-
-        """
-        if s == t:
-            return s
-
-        map_t = {}
-        
-        for char in t:
-            map_t[char] = 1 +map_t.get(char,0)
-
-        have,need = 0, len(map_t)
-        map_s = {}
+        t_map = {}
+        s_map = {}
+        substring_len = float('inf')
         min_substring = ""
         l = 0
 
+        for char in t:
+            t_map[char] = 1 +t_map.get(char,0)
+    
+        have = 0
+        need = len(t_map)
+
         for r in range(len(s)):
-            if s[r] in map_t:
-                map_s[s[r]] = 1 + map_s.get(s[r],0)
+            if s[r] in t_map:
+                s_map[s[r]] = 1 + s_map.get(s[r], 0)
             
-            if s[r] in map_t and map_s[s[r]] == map_t[s[r]]:
-                have +=1
+                if s_map[s[r]] == t_map[s[r]]:
+                    have +=1
     
             while have == need:
-                if not len(min_substring):
+                if r - l + 1 < substring_len:
                     min_substring = s[l:r+1]
-                elif min_substring and r - l + 1 < len(min_substring):
-                    min_substring = s[l:r + 1]
+                    substring_len = r - l + 1
 
-                if s[l] in map_s:
-                    map_s[s[l]] -=1
-                    if map_s[s[l]] < map_t[s[l]]:
+                if s[l] in s_map:
+                    s_map[s[l]] -=1
+                    if s_map[s[l]] < t_map[s[l]]:
                         have -=1
-                l +=1 
+                
+                l +=1
 
         return min_substring
 
 
-
+        
