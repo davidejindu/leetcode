41c0b3d51bb2ab5a30952b8489126406b886464c
David Ejindu <ejindudavid540@gmail.com>
2026-06-23 10:14:30 -0400

    Time: 111 ms (38.66%), Space: 19.6 MB (84.69%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,32 +1,43 @@
+"""
+
+AABABBA    k = 1
+l
+    r
+
+5 - 3
+max_replacement = 2
+l = 0
+
+{A:2, B:0}
+
+window = 1
+so the only time i have to move l is if the size of window - 2 is greater than k
+at that point i want to get the max size
+
+
+so i need to get the longest substring of the same character
+so i need a sliding window
+i want to make a hashmap where the key is the letter and the value is the frequency
+now i want to get the letter that appears most in that window and see if i can switch it to k then get the len
+
+
+
+"""
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        """"
-        AABABBA         k = 1
-        l
-        r
-
-         A:2, B:1 
-         length = 3
-          3 - 2 =< k
-         max = 3
-
-        keep getting the max values in hashmap
-        if the max value in hashmap 
-        get r - l + 1 - max_value in hashmap > l +=1 else get max lenght
-
-
-    """
-
-        window_map = {}
-        l = max_freq = max_replacement = 0
+        letter_map = {}
+        max_replacement = 0
+        l = 0
 
         for r in range(len(s)):
-            window_map[s[r]] = 1 + window_map.get(s[r], 0)
-            max_freq = max(window_map.values())
-            if (r - l + 1) - max_freq <= k:
-                max_replacement = max(max_replacement, r - l + 1)
-            else:
-                window_map[s[l]] -=1
+            letter_map[s[r]] = 1 + letter_map.get(s[r], 0)
+            window = r - l + 1
+            max_freq = max(letter_map.values())
+            if k < window - max_freq:
+                letter_map[s[l]] -=1
                 l +=1
 
+            max_replacement = max(max_replacement, r - l + 1)
+
         return max_replacement
+        
