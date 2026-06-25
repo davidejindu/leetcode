50658c701bc045a3fdd7f7bbacb0b4f839ecca7a
David Ejindu <ejindudavid540@gmail.com>
2026-06-25 14:04:53 -0400

    Time: 42 ms (97.64%), Space: 22.5 MB (54.26%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,21 +6,13 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        """
-    have two nodes
-    one slow 
-    one fast
-    if they are equal then we know there is a cycle since fast one shouldnt be same as slow unless there is a cycle
-
-        """
-
         fast = slow = head
 
         while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
 
-            if slow == fast:
+            while fast == slow:
                 return True
 
         return False
