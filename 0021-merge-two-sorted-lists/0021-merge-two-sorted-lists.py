57d6ad43716de91a6c59ef079696789bf0271d7c
David Ejindu <ejindudavid540@gmail.com>
2026-06-25 13:53:27 -0400

    Time: 0 ms (100%), Space: 19.1 MB (99.23%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -3,40 +3,33 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+"""
+so make a linkedlist that merges the two linked lists in order
+so create a dummy node that keeps track of the beggining
+then loop through each linked list and if one is smaller add it then move on
+
+
+"""
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        """
-        create a dummy node to keep track of head of the new list
-        then with the curr that will be used to create a new list
-        
-        loop while you have both nodes 
-        compare the two nodes if node1 is smaller than node 2
-        add curr.next to node1 and increment node1
-        else add node2 to curr.next increment node2
 
-        now if not node1 then set the whole rest of curr.next to that node
-
-
-        """
         dummy = ListNode()
         curr = dummy
-        node1, node2 = list1, list2
 
-        
-        while node1 and node2:
-            if node1.val > node2.val:
-                curr.next = node2
-                node2 = node2.next
+        while list1 and list2:
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
             else:
-                curr.next = node1
-                node1 = node1.next
+                curr.next = list2
+                list2 = list2.next
 
             curr = curr.next
 
-        if not node1:
-            curr.next = node2
-        else:
-            curr.next = node1
+        if list1:
+            curr.next = list1
+        elif list2:
+            curr.next = list2
 
         return dummy.next
-
+        
