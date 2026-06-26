7880ed32ade98fdb7c444d6b415852c5a1b8994e
David Ejindu <ejindudavid540@gmail.com>
2026-06-26 14:17:38 -0400

    Time: 3 ms (52.37%), Space: 27.7 MB (79.85%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -3,46 +3,57 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+"""
+
+input: 1 2 3 4
+               
+output 1 4 2 3 
+
+
+input: 1 2 | 5 4 3
+         ^
+             ^
+
+1 -> 5 -> 2 -> 4 -> 3
+
+output 1 5 2 4 3
+
+so first get the middle half of the list
+
+after you get the middle half you want to reverse the second half of the list
+"""
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
-        
-        """
-
-        1 -> 2 -> 3   4 <- 5
-            f
-                      s
-        1 -> 5 -> 2
-
-        """
-
-        
-        slow, fast = head, head.next
+        fast = slow = head
 
         while fast and fast.next:
-            slow = slow.next
             fast = fast.next.next
-        
-        second = slow.next
-        slow.next = None
+            slow = slow.next
 
+        second = slow.next 
+        slow.next = None
         prev = None
 
         while second:
             tmp = second.next
             second.next = prev
             prev = second
-            second = tmp
+            second = tmp 
 
-        first, second = head, prev
+        second = prev
+        first = head
+
+
 
         while second:
             tmp1, tmp2 = first.next, second.next
             first.next = second
             second.next = tmp1
-            first, second = tmp1, tmp2
+            first = tmp1
+            second = tmp2
 
-
+        return head
         
