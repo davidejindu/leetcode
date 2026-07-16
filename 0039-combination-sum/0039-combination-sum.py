8eb5b4956a43fe776f7fc6335270b2c5ce0466b8
David Ejindu <ejindudavid540@gmail.com>
2026-07-15 21:18:41 -0400

    Time: 23 ms (9.55%), Space: 19.5 MB (82.05%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,22 +1,57 @@
+"""
+create a list of all unique combinations that sum to the target
+
+same number can be chosen from the canidates an unlimited amount of time
+
+backtracking
+
+if the current sum is greater than the target return 
+
+else keep appending that number
+
+if value greater than target return
+
+if i == len(candidates) return 
+
+if value is equal to target then append to result array
+also want to pop and remove the last value probably
+
+candidates = [2,3,6,7]
+subset = [2,2]
+result = []     target = 7
+
+
+backtrack(0,6)
+backtrack(0,4)
+backtrack(0,2)
+backtrack(0,0)
+"""
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        result = []
+        candidates.sort()
+        result, subset = [], []
 
-        def dfs(i,cur,total):
-            if total == target:
-                result.append(cur.copy())
-                return 
-
-            if total > target or i == len(candidates):
+        def backtrack(i, value):
+            if i == len(candidates):
                 return
 
-            cur.append(candidates[i])
-            dfs(i,cur,total + candidates[i])
+            if value > target:
+                return 
 
-            cur.pop()
-            dfs(i+1,cur,total)
+            if value == target:
+                result.append(subset.copy())
+                return
 
-        dfs(0,[],0)
-        return result
+            subset.append(candidates[i])
+            backtrack(i, value + candidates[i])
+
+            subset.pop()
+            backtrack(i + 1, value)
 
             
+        backtrack(0,0)
+
+        return result
+
+
+        
