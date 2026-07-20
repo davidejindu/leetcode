d5e524b5b54e9c12b672431342885b4c5799dbc6
David Ejindu <ejindudavid540@gmail.com>
2026-07-19 22:04:43 -0400

    Time: 253 ms (37.72%), Space: 31 MB (10.72%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,36 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        """
-        can just dfs and check if the r c is in range and then check if its 1
-        if its 1 mark it as visited
-        the goal is to mark the neighboring 1's as visited in dfs so you dont revisit it
-
-
-
-        """
-        visited = set()
-        ROW, COL = len(grid), len(grid[0])
-        directions = [[1,0],[-1,0],[0,1],[0,-1]]
         islands = 0
+        ROWS, COLS = len(grid), len(grid[0])
+        visited = set()
+
 
         def dfs(r,c):
+            if (0 > r or r >= ROWS or
+                0 > c or c >= COLS or
+                grid[r][c] != "1" or
+                (r,c) in visited):
+                return
+
             visited.add((r,c))
 
-            for dr,dc in directions:
-                nr, nc = dr + r, dc + c
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c+1)
+            dfs(r,c-1)
 
-                if (0 <= nr and nr < ROW and
-                    0 <= nc and nc < COL and
-                    (nr,nc) not in visited and
-                    grid[nr][nc] == "1"):
-                    dfs(nr,nc)
-
-
-
-        for r in range(ROW):
-            for c in range(COL):
-                if grid[r][c] =="1" and (r,c) not in visited:
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1" and (r,c) not in visited:
                     islands +=1
                     dfs(r,c)
 
+
         return islands
+        
