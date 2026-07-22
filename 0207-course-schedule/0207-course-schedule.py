cd282d2c5eb712687dd3d8c01384ee4e8350ebb5
David Ejindu <ejindudavid540@gmail.com>
2026-07-22 11:52:23 -0400

    Time: 7 ms (44.42%), Space: 21.6 MB (11.23%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,56 +1,33 @@
-"""
-
-total # of courses labeled from 0 to # of courses
-
-given an array [0,1] where i need to take 0 if i want to take 1
-
-input: how many courses, array mapping courses i need to take 
-
-output: if i can take all the courses
-
-the only way i wouldnt be able to finish a course if there is a cycle
-
-so i need to create an adjancy list
-do a dfs where i add the node to a visit list and then check its neighbors
-if the neighbor is in visited list it means ive looped so i return False
-
-i keep going till i reach end and keep returning true at the end
-
-[1:0]
-
-"""
-
-
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        courseMap = defaultdict(list)
-        visiting,visited = set(), set()
+        graph = defaultdict(list)
 
-
-        #make an adjancy list the course is key and preq is value
         for course, preq in prerequisites:
-            courseMap[course].append(preq)
+            graph[course].append(preq)
 
+        visited, visiting = set(), set()
 
-        def dfs(course):
-            if course in visiting:
+        def cycle(node):
+            if node in visiting:
                 return False
 
-            if course in visited:
+            if node in visited:
                 return True
 
-            visiting.add(course)
+            visiting.add(node)
 
-            for preq in courseMap[course]:
-                if not dfs(preq):
+            for neighbor in graph[node]:
+                if not cycle(neighbor):
                     return False
 
-            visiting.remove(course)
-            visited.add(course)
+            visiting.remove(node)
+            visited.add(node)
             return True
 
-        for i in range(numCourses):
-            if not dfs(i):
+        for course in range(numCourses):
+            if not cycle(course):
                 return False
 
         return True
+
+        
