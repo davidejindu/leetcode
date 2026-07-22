e237df0381b990ab48af3ebd12144440a2f66964
David Ejindu <ejindudavid540@gmail.com>
2026-07-22 15:42:04 -0400

    Time: 0 ms (100%), Space: 19.4 MB (16.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,13 +1,27 @@
+"""
+
+want to know how many unique ways you can reach the top
+
+we know that if the top is 1 it takes 1 2 it takes 2 and 3 it takes 3
+
+we can recurse so if we are at 4 we know 3 is 3 unique ways and 4 - 3 is 1 so then we know thats 
+already in our memo so its 4 
+
+
+
+
+"""
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = {1:1, 2:2}
+        memo = {1:1, 2:2, 3:3}
 
         def climb(x):
             if x in memo:
                 return memo[x]
             else:
-                memo[x] = climb(x - 1) + climb(x - 2)
+                memo[x] = climb(x - 2) + climb(x - 1)
+                return memo[x]
 
-            return memo[x]
 
         return climb(n)
+        
