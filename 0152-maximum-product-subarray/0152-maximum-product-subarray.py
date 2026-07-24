0e3da8ce71dd2137e380790368e98dccd93587aa
David Ejindu <ejindudavid540@gmail.com>
2026-07-24 16:04:47 -0400

    Time: 7 ms (38.73%), Space: 19.8 MB (77.85%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,21 +1,38 @@
+"""
+
+find subarray that has the largest prlduct and return the product
+Input: 2,3,-2,4         Output: 6
+
+we need to keep track of the max and min occurance we have
+because if we get a negative value then the min will more than likely turn into the max
+
+also if the value in the array is ever 0 we should reset max and min variables to 1
+
+2 3 -2 4
+     ^
+maxx = -2
+minn = -12
+max_product = 6
+temp = 2
+
+
+"""
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
+        max_product = max(nums)
 
-        result = max(nums)
+        minn = 1
+        maxx = 1
 
-        maxx, minn = 1,1 
-
-        for n in nums:
-            if n == 0:
+        for val in nums:
+            if val == 0:
                 maxx = minn = 1
+            temp = val * maxx
 
-            tmp = maxx * n
-            maxx = max(maxx * n, minn * n, n)
-            minn = min(tmp, minn * n, n)
+            maxx = max(val, temp, minn * val)
+            minn = min(val, temp, minn * val)
 
-            result = max(result, maxx)
+            max_product = max(max_product, maxx)
 
-        return result
-
-            
+        return max_product
         
