5c5d6216c328b489492e54864575ee71479de026
David Ejindu <ejindudavid540@gmail.com>
2026-07-27 18:30:57 -0400

    Time: 207 ms (6.9%), Space: 47 MB (98.93%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -1,27 +1,33 @@
 """
-find minimum overlapping intervals that can be removed so no intervals overlap
+return minimum # of intervals i need to remove to make the intervals non overlapping
 
-so the goal is first sort it 
+intervals which touch are non overlapping
 
+definetely have to sort first before anything 
 
-[1,2], [1,3], [2,3],[3,4]
+[1,2],[1,3],[2,3],[3,4]
+        ^
 
-start at 1 and if it overlaps with element behind it whatever interval has larger end
-remove it then increment
+count = 1
+yo so basically if you find an overlap increment count to 1 but then you want to 
+make sure whatever end is smaller becomes i since its less likely to overlap
+  
+
 
 """
-
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key= lambda x: x[0])
         count = 0
+        intervals.sort()
 
         for i in range(1, len(intervals)):
-            if intervals[i][0] < intervals[i - 1][1]:
-                count += 1
-                if intervals[i-1][1] < intervals[i][1]:
-                    intervals[i] = intervals[i - 1]
-
-        return count 
+            if intervals[i][0] < intervals[i-1][1]:
+                count +=1
+                intervals[i] = [
+                    min(intervals[i][0],intervals[i-1][0]),
+                    min(intervals[i][1],intervals[i-1][1])
+                ]
+                
 
         return count
+        
