113bb5af37277aa0104bc8813b0eccc552ff4e59
David Ejindu <ejindudavid540@gmail.com>
2026-07-27 14:51:22 -0400

    Time: 12 ms (11.49%), Space: 22.3 MB (97.49%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,32 +1,18 @@
-"""
-your given an array of intergers
-
-some of the elements overlap
-return the array with merging intervals so non overlap
-
-[1,3],[1,6],[8,10],[15,18]
-                     ^
-                          
-
-result = [[1,6]]
-
-"""
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        result = []
         intervals.sort()
+        result = []
 
         for i in range(1, len(intervals)):
             if intervals[i][0] > intervals[i-1][1]:
                 result.append(intervals[i-1])
             else:
                 intervals[i] = [
-                    min(intervals[i-1][0], intervals[i][0]),
-                    max(intervals[i-1][1], intervals[i][1])
-                    ]
+                    min(intervals[i-1][0],intervals[i][0]),
+                    max(intervals[i-1][1],intervals[i][1])
+                ]
 
-        result.append(intervals[len(intervals) - 1])
-
+        result.append(intervals[-1])
         return result
-    
+
         
