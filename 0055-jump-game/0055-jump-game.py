5321dab0194db95bf5bd9fb3027f9117f6974757
David Ejindu <ejindudavid540@gmail.com>
2026-07-27 20:47:07 -0400

    Time: 8 ms (96.06%), Space: 20.2 MB (62.65%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,12 +1,35 @@
+"""
+
+each value in the array represents maximum jump length at that index
+
+return true if you can reach the last index with jumps
+
+input: [2, 3, 1, 1, 1, 4]
+                    ^
+
+jump = i + nums[i] 
+index = 1
+output: true
+
+3,2,1,0,4
+        ^
+jump = 3
+
+
+
+"""
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        max_reach = 0
+
+        jump = 0
 
         for i in range(len(nums)):
-            if i > max_reach:
+            if i > jump:
                 return False
 
-            max_reach = max(max_reach, i + nums[i])
+            jump = max(jump, nums[i] + i)
 
-            if max_reach >= len(nums) - 1:
+            if jump >= len(nums) - 1:
                 return True
+            
+        
