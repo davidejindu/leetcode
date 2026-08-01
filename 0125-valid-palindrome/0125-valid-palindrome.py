4e5aa7b21aa02253f834e84b4d3f31cb41e19122
David Ejindu <ejindudavid540@gmail.com>
2026-08-01 11:53:11 -0400

    Time: 11 ms (31.66%), Space: 19.4 MB (99.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,30 +1,26 @@
 """
-A man, a plan, a canal: Panama
-l               
-            
 
+return true if palindrome or not
+
+basically two pointers one at end other at beginning
+while isalnum() which means its an alphabet or number you are going to check if lower is equal
 
 """
-
-
-
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         l, r = 0, len(s) - 1
 
-
         while l < r:
             while l < r and not s[l].isalnum():
-                l+=1
+                l +=1
 
-            while l < r and not s[r].isalnum():
+            while l < r and not s[r].isalnum(): 
                 r -=1
 
             if s[l].lower() != s[r].lower():
                 return False
 
-            l +=1
             r -=1
+            l +=1
 
-        return True
-        
+        return True        
