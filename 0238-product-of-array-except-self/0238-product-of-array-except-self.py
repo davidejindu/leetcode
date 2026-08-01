48b19deda7e41eb63139cafa82dbba90056be6fa
David Ejindu <ejindudavid540@gmail.com>
2026-08-01 09:16:04 -0400

    Time: 19 ms (74.27%), Space: 25.6 MB (54.66%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,43 +1,58 @@
 """
+return an array where the index is equal to the product of all the elements of nums except itself
 
-to get the product of each array except itself in i you have to get the postfrix and prefix
+to get the product of all the elements of nums except itself you need to prefix and postfix
 
-start of by getting the prefix 
+to get prefix of first index default it to 1
 
-if we are starting at beginning the prefix will just be 1 since we cant multiply by 0
+input: [1,2,3,4]
+output: [24,12,8,6]
 
-1 1 2 6
-this would be prefix
+prefix = [1,1,2,6]
 
-create a prefix array just intialize it to all 1
+to get prefix first set the array to all 1s 
+since we know first index has to be one
 
-then start at index 1 and multiply it by 1 and make the new prefix be prefix * nums[i -1]
+nums = 1 2 3 4
+           i
+
+pre =  1 1 2 6
+             i
+
+prefix = 6
+have a prefix variable that starts off as 1 and multiple nums[i - 1] to that prefix and 
+set prefix[i] = prefix
 
 
-1,2,3,4
-  ^
-1 12 8 6
-^
+now same thing but postfix
+postfix variable starts off as 1
 
-postfix = 12
+we are going to multiple the prefix array
+
+nums    = 1 2 3 4
+                i
+postfix = 1 1 2 6
+              i
+postfix = 24
+
+
 
 """
-
-
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        result = [1] * len(nums)
         prefix = 1
+        prefix_arr = [1] * len(nums)
 
         for i in range(1, len(nums)):
             prefix *= nums[i - 1]
-            result[i] = prefix
+            prefix_arr[i] = prefix
+
 
         postfix = 1
 
-        for i in range(len(nums) -2, -1, -1):
+        for i in range(len(nums) - 2, -1, -1):
             postfix *= nums[i + 1]
-            result[i] *= postfix
+            prefix_arr[i] *= postfix
 
-        return result
-
+        return prefix_arr
+        
