d48b3600f8c90e633f01c9459c3344801a555c97
David Ejindu <ejindudavid540@gmail.com>
2026-08-01 12:38:28 -0400

    Time: 61 ms (34.4%), Space: 29.8 MB (5.3%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -1,19 +1,35 @@
+"""
 
+find two lines such that the can contain the most water
+
+return the max water a container can store
+
+the max water a container can store in the min height of the two lengths * distance apart
+have two pointers and do that
+move pointer to whatever heigh is smaller so you can keep max of the two lines
+
+
+
+"""
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        maxHeight = 0
-        l,r = 0, len(height) - 1
+
+        maxx = 0
+        l, r = 0, len(height) - 1
 
         while l < r:
-            width = r - l 
-            minHeight = min(height[l], height[r]) 
+            minHeight = min(height[l], height[r])
 
-            maxHeight = max(maxHeight, minHeight * width)
+            current_height = (r - l) * minHeight
+
+            maxx = max(current_height, maxx)
 
             if height[l] < height[r]:
                 l +=1
             else:
                 r -=1
 
-        return maxHeight
+        return maxx
+
+        
         
