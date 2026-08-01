dbdf8d3365d12545db3187d2e0731171c8d34e1a
David Ejindu <ejindudavid540@gmail.com>
2026-08-01 08:08:43 -0400

    Time: 8 ms (34.35%), Space: 24.7 MB (20.7%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,38 +1,50 @@
 """
-return the the most k freq elements 
-make an array that hold where the max frequency possible
+return k most frequest elements 
 
-inside of each element append to the index if it matches the freq the  element appears
-
-get that by creating a hashmap of the number with the value being its freq
+input : [1,1,1,2,2,3], k = 2
+output: 1,2
 
 
-after you store each number in the correct freq loop backwards until you have 
-appended k elements to the result
+so we want first to keep track of frequency so probably create hashmap
+key is the number and value is the count
+
+then we know that the max amount of frequency is the len of nums
+so create a freq array that is len nums + 1 and put the freq from hashmap into index
+make sure freq array each element is a list so you can appens numbers with same freq
+
+after that you want to loop backwards of freq and append to a result array until it 
+equals k
+
+map = {1:3, 2:2, 3:1}
+
+freq = [[], [3], [2], [1], [], [], []]
+loop backwards and you will append
+[1,2]
 
 
 
 """
-
-
-
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq = [[] for i in range(len(nums) + 1)]
+        freq_list = [[] for i in range(len(nums) + 1)]
+        freq_map = defaultdict(int)
 
-        freq_map = {}
+        #create freq map
+        for number in nums:
+            freq_map[number] +=1
 
-        for num in nums:
-            freq_map[num] = 1 + freq_map.get(num, 0)
-
+        #put freq of number in correct spot in freq list
         for num, count in freq_map.items():
-            freq[count].append(num)
+            freq_list[count].append(num)
 
+        #append to get order
         result = []
 
+
+        #loop starting at end of freq list to get highest freq first and append to result
         for i in range(len(nums), -1, -1):
-            for nums in freq[i]:
-                result.append(nums)
+            for number in freq_list[i]:
+                result.append(number)
                 if len(result) == k:
                     return result
 
