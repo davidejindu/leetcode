6eb7f2490e4d321203ae4fcf398e149861a14995
David Ejindu <ejindudavid540@gmail.com>
2026-08-03 20:32:18 -0400

    Time: 15 ms (80.45%), Space: 19.4 MB (18.09%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,46 +1,49 @@
+"""
+
+
+okay so a permutation is when all the characters in s1 show up in substring
+of characters in s2
+
+so what you want to do is have a hashmap of s1
+
+and you want a sliding window to see if in that window the two hashmaps are the same
+
+s1 = "ab", s2 = "eidbaooo"
+
+"eidbaooo"
+    l
+     r
+s1_map = {a:1, b:1}
+s2_map = {b:1, a:1}
+if len of r - l + 1 = len(s1) 
+check if s1_map == s2_map if not removing s[l] and shift pointer
+
+"""
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        """
-        create a hashmap for s1
-        so do sliding window hashmap where you compare the len of s1
-        with hashmap of s1
-
-       """
-
+        if len(s1) > len(s2):
+            return False
 
         s1_map = defaultdict(int)
         s2_map = defaultdict(int)
-        l = 0
-
-        if len(s1) > len(s2) :
-            return False
 
         for char in s1:
-            s1_map[char] += 1
+            s1_map[char] +=1
 
-        """
-         abcdeabcdx
-         l
-          r
-        s1_map = {a:2,b:2, c:2, d:2, x:1, e:1}
-        s2_map = {a:2, b:2, c:2,d:2, e:1, x:1}
-  
-        """
+        l = 0
 
         for r in range(len(s2)):
             s2_map[s2[r]] +=1
-    
 
-            if r - l + 1 > len(s1) and not s2_map == s1_map:
-                 s2_map[s2[l]] -=1
-                 if s2_map[s2[l]] == 0:
-                    s2_map.pop(s2[l])
-                 l +=1
-
-          
-
-            if s2_map == s1_map:
-                return True
+            if r - l + 1 == len(s1):
+                if s1_map == s2_map:
+                    return True
+                else:
+                    s2_map[s2[l]] -=1
+                    if s2_map[s2[l]] == 0:
+                        s2_map.pop(s2[l])
+                    l +=1
 
 
         return False
+        
