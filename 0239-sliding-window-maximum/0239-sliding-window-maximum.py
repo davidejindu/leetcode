a447ce9177db68cc877d200c5eac00eacb122531
David Ejindu <ejindudavid540@gmail.com>
2026-08-03 21:26:42 -0400

    Time: 365 ms (10.07%), Space: 47 MB (5.08%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,40 +1,21 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        """
-        make a deque for O(n)
-        if the queue isnt empty and the last value in queue is less than nums[r] pop
-        after that append to the queue so it has the top most value
-        1,3,-1,-3,5,3,6,7  k = 3
-          l
-             r
-
-        queue [1]        result = [3] 
-        """
-
-        queue = collections.deque()
+        maxHeap = []
         result = []
-        l = 0
 
         for r in range(len(nums)):
-            while queue and nums[queue[-1]] < nums[r]:
-                queue.pop()
-            queue.append(r)
+            heapq.heappush(maxHeap, (-nums[r], r))
 
-            if r - l + 1 == k:
-                result.append(nums[queue[0]])
-                l +=1
+            if r < k - 1:
+                continue
 
-            if l > queue[0]:
-                queue.popleft()
-            
+            last_index = r - k + 1
+
+            while maxHeap and maxHeap[0][1] < last_index:
+                heapq.heappop(maxHeap)
+
+            result.append(-maxHeap[0][0])
+
 
         return result
-
-
-
-
-
-     
-
-
         
