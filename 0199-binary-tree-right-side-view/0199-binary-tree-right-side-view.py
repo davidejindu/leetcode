b12fcf953f5ec99b1782515cf39b89d41f1ddce1
David Ejindu <ejindudavid540@gmail.com>
2026-08-12 15:14:11 -0400

    Time: 0 ms (100%), Space: 19.5 MB (9.03%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -4,27 +4,54 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+"""
+
+want to return the values of nodes i can see from the right side
+
+so want to keep track of the level the node is on and the node 
+
+i want to append the right node first and then left if it exists
+
+so i want to append the first node from that level to the result array
+then pop the other nodes that have that level from the array after first adding its children
+to the queue
+
+    
+queue = [(1,2)]
+result = [1,3]
+level = 1
+self.level = 2
+
+
+"""
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        queue = collections.deque()
+        if not root:
+            return []
 
-        queue.append(root)
+        self.level = 0
+        queue = deque([root])
         result = []
 
 
         while queue:
-            rightSide = None
+            level_size = len(queue)
 
-            for _ in range(len(queue)):
+            for i in range(level_size):
                 node = queue.popleft()
 
-                if node: 
-                    rightSide = node
-                    queue.append(node.left)
+                if i == 0:
+                    result.append(node.val)
+
+                if node.right:
                     queue.append(node.right)
 
-            if rightSide:
-                result.append(rightSide.val)
+                if node.left:
+                    queue.append(node.left)
 
 
         return result
+
+            
+
+           
