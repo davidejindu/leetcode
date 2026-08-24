7e25e6ba3d36b4abcfac396e37e62a997829150b
David Ejindu <ejindudavid540@gmail.com>
2026-08-24 17:19:12 -0400

    Time: 4 ms (13.63%), Space: 22.1 MB (87.92%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -4,19 +4,30 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+"""
+stack and heap
+when heap == len(k) return heap[0]
+
+k = 1
+i dont even need a heap since its inorder traversal and a bst so just an array and return
+last index when len == k
+
+"""
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        count = 0
-        stack = []
+        result, stack = [], []
         node = root
 
-        while node or stack:
+        while stack or node:
             while node:
                 stack.append(node)
                 node = node.left
 
             node = stack.pop()
-            count +=1
-            if count == k:
-                return node.val
+            result.append(node.val)
+
+            if len(result) == k:
+                return result[-1]
+
             node = node.right
+        
