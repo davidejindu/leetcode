57e1fb7e25b967bb9ae3b0c92f7b233c497bd187
David Ejindu <ejindudavid540@gmail.com>
2026-08-24 10:37:11 -0400

    Time: 4 ms (23.49%), Space: 22.5 MB (40.51%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -4,14 +4,26 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+"""
+
+so you need to dfs down to the bottom and start counting from bottom to top
+
+if the node is none you return 0
+
+you want to return the height of the max left or right + 1
+
+"""
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
 
-        if not root:
-            return 0
+        def dfs(node):
+            if not node:
+                return 0
 
-        left = 1 + self.maxDepth(root.left)
-        right = 1 + self.maxDepth(root.right)
+            left = dfs(node.left)
+            right = dfs(node.right)
 
-        return max(left,right)
+            return 1 + max(left,right)
+
+        return dfs(root)
         
