ad93affb5d13aa5a406c1cbcef6dd6cbc538be2b
David Ejindu <ejindudavid540@gmail.com>
2026-08-31 09:56:47 -0400

    Time: 82 ms (39.87%), Space: 90.9 MB (22.38%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -4,31 +4,17 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-"""
-
-use the preorder and inorder arrays to construct the binary tree
-
-3,9,20,15,7
-  ^ 
-9 = root
-
-
-9,3,15,20,7
-  ^
-we know that the first value in preorder is the root then we 
-know everything till we reach the index of that root in inorder array is on the left of binary tree 
-
-"""
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         if not preorder or not inorder:
-            return 
+            return
 
-        root = TreeNode(preorder[0])
-        index = inorder.index(root.val)
-        root.left = self.buildTree(preorder[1:index+1],inorder[:index])
+        root_val = preorder[0]
+        root = TreeNode(root_val)
+        index = inorder.index(root_val)
+
+        root.left = self.buildTree(preorder[1:1+index], inorder[:index])
         root.right = self.buildTree(preorder[index+1:],inorder[index+1:])
+
         return root
-
-
         
